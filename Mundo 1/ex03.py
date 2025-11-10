3cbbdcc64c0bf640dfdc1793abc107878538c2e8
Davi <davivercezeramos@gmail.com>
2025-11-10 18:54:03 -0300

    Adiciona exercícios 3, 4 e 5 do Mundo 1

---
+++ Mundo 1/ex03.py
@@ -0,0 +1,4 @@
+n1 = int(input('Digite o primeiro valor para a soma: '))
+n2 = int(input('Digite o segunda valor para a soma: '))
+resultado = (n1 + n2)
+print(f'A soma entre {n1} e {n2} é igual a {resultado}')
---
+++ Mundo 1/ex05.py
@@ -0,0 +1,5 @@
+n = int(input('Digite um número: '))
+sucessor = (n + 1)
+antecessor = (n - 1)
+
+print(f'Analisando o valor {n}, seu antecessor é {antecessor} e seu sucessor é {sucessor}')
